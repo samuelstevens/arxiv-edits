4cc7f310588da54b253143ef39263faf2a58b44f
Sam Stevens <stevens.994@osu.edu>
2019-10-23 15:09:42 -0400

    new filenames

--- arxivedits/tex.py
+++ arxivedits/tex.py
@@ -30,63 +30,6 @@ def download_file(url: str, local_filename: str) -> str:
     return local_filename
 
 
-def download_tex_src(arxiv_id: ArxivID, clean=False, directory='data') -> List[str]:
-    tex_sources: List[str] = []
-
-    abs_url = f'https://arxiv.org/abs/{arxiv_id}'
-
-    # 1. get the https://arxiv.org/abs/ page
-    print(f'Getting {abs_url}')
-    with requests.get(abs_url, cookies=COOKIES) as r:
-        r.raise_for_status()
-        abs_page = r.text
-
-    # 1.5 check if source is available.
-    source_available = SOURCE_PATTERN.search(abs_page)
-
-    if not source_available:
-        print(f'Source for {arxiv_id} not available.')
-        return tex_sources
-
-    # 2. find all the version tags on the page.
-    versions = VERSION_PATTERN.findall(abs_page)
-
-    # for each version:
-    for v in versions:
-        url = f'https://arxiv.org/e-print/{arxiv_id}v{v}'
-        filename = os.path.join(directory, f'{arxiv_id}v{v}')
-
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
-
-        # 1. download vX to the local filesystem
-        if not os.path.isfile(filename):
-            download_file(url, filename)
-
-        # 2. gunzip the .gz
-        with gzip.open(filename) as gz:
-            tex_source: str = ''
-
-            filetype: str = magic.from_buffer(gz.read(1024))
-
-            if 'ASCII' in filetype:
-                tex_source = gz.read().decode()
-            elif 'UTF-8' in filetype:
-                tex_source = gz.read().decode('utf_8', errors='replace')
-            elif 'tar' in filetype:
-                # 3. get the longest .tex file in each tar
-                tex_source = get_longest_tex(filename)
-            else:
-                print(f'Not sure what type {filetype} is.')
-
-        tex_sources.append(tex_source)
-
-        if clean and os.path.isfile(filename):
-            os.remove(filename)
-
-    return tex_sources
-
-
 def get_longest_tex(filename) -> str:
     tex = ''
     with tarfile.open(filename) as tar:
@@ -112,7 +55,7 @@ def detex(text: str) -> str:
         return ''
 
 
-def analyze_sources(directory, output_file, append=False):
+def analyze_source_types(directory, output_file, append=False):
     filenames = sorted(os.listdir(directory))
 
     with open(output_file, 'a' if append else 'w') as fout:
@@ -120,7 +63,7 @@ def analyze_sources(directory, output_file, append=False):
             filepath = os.path.join(directory, filename)
 
             if os.path.isdir(filepath):
-                analyze_sources(filepath, output_file, append=True)
+                analyze_source_types(filepath, output_file, append=True)
             else:
                 cmd = f'file -I {filepath}'
                 with os.popen(cmd) as fin:
@@ -136,11 +79,10 @@ def download_source_files(arxiv_id: ArxivID, version_count: int, output_director
 
     for v in range(1, version_count+1):
         url = f'https://arxiv.org/e-print/{arxiv_id}v{v}'
-        directory = os.path.join(output_directory, f'{arxiv_id}')
-        filename = os.path.join(directory, f'v{v}')
+        filename = os.path.join(output_directory, f'{arxiv_id}-v{v}')
 
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
+        if not os.path.isdir(output_directory):
+            os.makedirs(output_directory)
 
         if not os.path.isfile(filename):
             try:
@@ -166,9 +108,9 @@ def get_ids() -> List[Tuple[ArxivID, int]]:
 if __name__ == '__main__':
     ARXIV_ID_PAIRS = get_ids()
 
-    # for arxiv_id, version_count in ARXIV_ID_PAIRS:
-    #     download_source_files(arxiv_id, version_count)
+    for arxiv_id, version_count in ARXIV_ID_PAIRS:
+        download_source_files(arxiv_id, version_count)
 
     source_type_file = os.path.join('data', 'initial_types.txt')
     directory = os.path.join('data', 'source_files')
-    analyze_sources(directory, source_type_file)
+    analyze_source_types(directory, source_type_file)
