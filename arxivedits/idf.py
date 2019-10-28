ce65b6072419597b19fa4cd4df20186bde583385
Sam Stevens <stevens.994@osu.edu>
2019-10-28 15:05:13 -0400

    updated idf

--- arxivedits/idf.py
+++ arxivedits/idf.py
@@ -1,40 +1,13 @@
 from typing import List, Set
 import shelve
+import os
 
-from db import connection
-from structures import ArxivID
-from tex import detex, download_tex_src
+from db import connection, TEX_DB, IDF_DB
 from nlp import ArxivTokenizer
 
 import requests
 
 t = ArxivTokenizer()
-
-IDF_DB = 'data/idf'
-TEX_DB = 'data/tex'
-
-
-def ids() -> List[ArxivID]:
-    '''
-    Returns a pseudo-list of arxivIDs with multiple versions.
-    '''
-    query = 'SELECT arxiv_id FROM papers WHERE multiple_versions = 1'
-    con = connection()
-    con.row_factory = lambda cursor, row: row[0]
-    ids = con.execute(query)
-
-    return ids
-
-
-def get_src(arxiv_id: ArxivID) -> List[str]:
-    srcs = []
-
-    try:
-        srcs = download_tex_src(arxiv_id, clean=False)
-    except requests.exceptions.HTTPError as e:
-        print(f'{e}')
-
-    return srcs
 
 
 def split(text: str) -> List[str]:
@@ -45,62 +18,31 @@ def tokenize(sentence: str) -> List[str]:
     return t.split(sentence, group='word')
 
 
-def store_words(words: List[str]) -> None:
-    '''
-    Adds a list of words to the shelve dictionary
-    '''
-    with shelve.open(IDF_DB) as db:
-        for w in words:
-            w = w.lower()  # should words be converted to lowercase?
-            if w in db:
-                db[w] += 1
-            else:
-                db[w] = 1
+def adddoc(inputfile):
+    pass
+    # open and read the file
+    # tokenize the input into words
+    # create a set of added words
+    added_words = set()
+    with open(inputfile, 'r') as f:
+        content = f.read()
 
+    words = tokenize(content)
+    print(f'{inputfile} had {len(words)} words.')
 
-def store_tex(arxiv_id: ArxivID, tex_srcs: List[str]) -> None:
-    '''
-    Writes the tex source files for an arxiv id to storage.
-    '''
-
-    with shelve.open(TEX_DB) as db:
-        if arxiv_id in db:
-            print(f'{arxiv_id} is already in the db.')
-            print('Overwriting...')
-
-        db[arxiv_id] = tex_srcs
-
-
-def get_downloaded_ids() -> Set[ArxivID]:
-    with shelve.open(TEX_DB) as db:
-        return set(db.keys())
-
-
-def main():
-    arxiv_ids = list(ids())[0:15]
-
-    stored_ids = get_downloaded_ids()
-
-    print(f'{len(stored_ids)} out of {len(arxiv_ids)} already downloaded.')
-
-    for i in arxiv_ids:
-        if i in stored_ids:
-            # print(f'{i} has already been stored.')
-            continue
-
-        tex_srcs = get_src(i)  # This is the most likely to fail.
-
-        for version in tex_srcs:
-            clean_src = detex(version)
-
-            sentences = split(clean_src)
-
-            for s in sentences:
-                words = tokenize(s)
-                store_words(words)
-
-        store_tex(i, tex_srcs)
+    with shelve.open(IDF_DB) as idf:
+        for word in words:
+            if word not in added_words:
+                if word in idf:
+                    idf[word] += 1
+                else:
+                    idf[word] = 1
+                added_words.add(word)
 
 
 if __name__ == '__main__':
-    main()
+    textfiles = os.path.join('data', 'text')
+
+    for textfile in os.listdir(textfiles):
+        textfilepath = os.path.join(textfiles, textfile)
+        adddoc(textfilepath)
