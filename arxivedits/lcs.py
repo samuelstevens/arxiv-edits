f06698bc1c2b26b9e82ae9e20a1827c588dc6962
Sam Stevens <stevens.994@osu.edu>
2019-11-06 16:58:01 -0500

    updated lcs with fast implmentation

--- arxivedits/lcs.py
+++ arxivedits/lcs.py
@@ -4,7 +4,7 @@ Implements and exports a weighted-LCS algorithm.
 import shelve
 import os
 from math import log  # natural log
-from typing import List, Any
+from typing import List, Optional, TypeVar
 from nlp import ArxivTokenizer
 from db import IDF_DB
 
@@ -12,16 +12,22 @@ TOKENIZER = ArxivTokenizer()
 
 TOTALDOCS = len(os.listdir(os.path.join('data', 'unzipped')))
 
-DOCUMENTFREQ = {}
+DOCUMENTFREQ: Optional[shelve.DbfilenameShelf] = None
+
+Type = TypeVar('T')
 
 
-def idf(word) -> float:
+def idf(word: str) -> float:
     '''
     Returns the inverse document frequency of a word
     '''
     numerator = 1 + TOTALDOCS
 
-    if word not in DOCUMENTFREQ:
+    if not word:
+        print(f'Word {word} not a string.')
+        return -1 * float('inf')
+
+    if not DOCUMENTFREQ or word not in DOCUMENTFREQ:
         print(f'Word {word} not in document frequency.')
         return -1 * float('inf')
 
@@ -30,23 +36,24 @@ def idf(word) -> float:
     return log(numerator / denominator)  # natural log
 
 
-def lcs(sequence1: List[Any], sequence2: List[Any]) -> List[Any]:
+def lcs(seq1: List[Type], seq2: List[Type]) -> List[Type]:
     '''
-    Naive implementation of longest common subsequence. Implemented with an explanation from https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
+    longest common subsequence, inspired by https://en.wikipedia.org/wiki/Longest_common_subsequence_problem#Worked_example
     '''
-    if not sequence1 or not sequence2:
-        return []
 
-    if sequence1[-1] == sequence2[-1]:
-        return lcs(sequence1[:-1], sequence2[:-1]) + [sequence1[-1]]
+    table: List[List[List[Type]]] = [[[]] * (len(seq2) + 1)] * (len(seq1) + 1)
 
-    version1 = lcs(sequence1[:-1], sequence2)
-    version2 = lcs(sequence1, sequence2[:-1])
+    for i in range(len(seq1)):
+        for j in range(len(seq2)):
+            if i == 0 or j == 0:
+                table[i][j] = []
+            elif seq1[i] == seq2[j]:
+                table[i][j] = max(table[i][j-1], table[i-1]
+                                  [j], key=len) + [seq2[j]]
+            else:
+                table[i][j] = max(table[i][j-1], table[i-1][j], key=len)
 
-    if len(version1) > len(version2):
-        return version1
-
-    return version2
+    return table[len(seq1) - 1][len(seq2) - 1]
 
 
 def similarity(sentence1: str, sentence2: str) -> float:
@@ -72,14 +79,24 @@ def main():
     global DOCUMENTFREQ
     DOCUMENTFREQ = shelve.open(IDF_DB)
 
+    sample_lcs1 = ['Hi', 'my', 'name', 'is', 'Sam']
+    sample_lcs2 = ['Hello', 'my', 'name', 'is', 'Steve',
+                   'or', 'you', 'can', 'call', 'me', 'Sam']
+
+    print(lcs(sample_lcs1, sample_lcs2))
+
     sample1 = "The long-sought Higgs boson may soon be observed at the CERN Large Hadron."
     sample2 = "The long-sought Higgs boson particle will be observed at CERN."
 
     sample3 = "The long-sought Higgs boson may soon be observed at the CERN Large Hadron."
     sample4 = "It can not be determined correctly."
 
+    sample5 = "A precise theoretical understanding of the kinematic distributions for diphoton production in the standard model could provide valuable guidance in the search for the Higgs boson signal and assist in the important measurement of Higgs boson coupling strengths."
+    sample6 = "The long-sought Higgs boson may soon be observed at the CERN Large Hadron."
+
     print(similarity(sample1, sample2))
     print(similarity(sample3, sample4))
+    print(similarity(sample5, sample6))
 
     DOCUMENTFREQ.close()
 
